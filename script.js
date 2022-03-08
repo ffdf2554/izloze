174cf760257b65d9728a0b6baf73023db2f87b78
ffdf2554 <demjanovs.a@gmail.com>
2022-03-08 10:18:18 +0200

    pirmais
    test

---
+++ script.js
@@ -0,0 +1,5 @@
+const vardi= ['Andris','Liene','Marija','Kristers','Mairis'];
+const balvas=['Laptop','PS5','Iphone','Televizors','PC'];
+const naudaKopa= 100000;
+let uzvaretajuSkaits= 5;
+console.log(vardi[rand]);
