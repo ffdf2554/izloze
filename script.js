adacceda2a1815c45e15037df79005e721087230
ffdf2554 <demjanovs.a@gmail.com>
2022-03-08 10:22:52 +0200

    2

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const vardi= ['Andris','Liene','Marija','Kristers','Mairis'];
 const balvas=['Laptop','PS5','Iphone','Televizors','PC'];
 const naudaKopa= 100000;
 let uzvaretajuSkaits= 5;
-Marh.random=math.random()*10;
+Math.random=math.random()*10;
 rand=Math.floor(rand);//apaļo lejā
 console.log(vardi[rand]);
