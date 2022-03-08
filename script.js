5f7ac913bc0f8798414737d05ec786f994f15711
ffdf2554 <demjanovs.a@gmail.com>
2022-03-08 13:15:22 +0200

    otrdiena 2

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-const vardi= ['Andris','Liene','Marija','Kristers','Mairis'];
-const balvas=['Laptop','PS5','Iphone','Televizors','PC'];
-const naudaKopa= 100000;
-let uzvaretajuSkaits= 5;
-let rindas=document.querySelector('rindas');//pievienot mainīgo
-for (let i=0;i<uzvaretajuSkaits;i++){
-let rand=Math.random()*vardi.length;//vārdu skaits
-rand=Math.floor(rand);//apaļo lejā
-let uzvaretajs=vardi[rand];
-console.log(vardi[rand]);
-}
-rindas.innerHTML+=`
+const vardi = ['Andris', 'Liene', 'Marija', 'Kristers', 'Mairis'];
+const balvas = ['Laptop', 'PS5', 'Iphone', 'Televizors', 'PC'];
+const naudaKopa = 100000;
+let uzvaretajuSkaits = 5;
+let rindas = document.querySelector('.rindas');//pievienot mainīgo
+rindas.innerHTML = ' ';
+let uz2 = document.querySelector('.balvas');//pievienot mainīgo
+balvas.innerHTML = ' ';
+
+for (let i = 0; i < uzvaretajuSkaits; i++) {
+    let rand = Math.random() * vardi.length;//vārdu skaits
+    rand = Math.floor(rand);//apaļo lejā
+    let uzvaretajs = vardi[rand];
+    let uz2 = balvas[rand];
+    //console.log(vardi[rand]);
+
+    rindas.innerHTML += `
 <tr>
-    <td>${i+1}</td>
+    <td>${i + 1}</td>
     <td>${uzvaretajs}</td>
+    <td>${uz2}</td>
+    </tr>
     `
+}
