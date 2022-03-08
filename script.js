55328bcd2a4306f73060a4b0a85c45f7b6f55316
ffdf2554 <demjanovs.a@gmail.com>
2022-03-08 11:14:34 +0200

    otrdiena 1

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const vardi= ['Andris','Liene','Marija','Kristers','Mairis'];
 const balvas=['Laptop','PS5','Iphone','Televizors','PC'];
 const naudaKopa= 100000;
 let uzvaretajuSkaits= 5;
-Math.random=math.random()*10;
+let rindas=document.querySelector('rindas');//pievienot mainīgo
+for (let i=0;i<uzvaretajuSkaits;i++){
+let rand=Math.random()*vardi.length;//vārdu skaits
 rand=Math.floor(rand);//apaļo lejā
+let uzvaretajs=vardi[rand];
 console.log(vardi[rand]);
+}
+rindas.innerHTML+=``
